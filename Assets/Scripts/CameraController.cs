37af1a2dbfeebe9f2021488f4c63080582f7a107
Garman12 <arch3m1d35@gmail.com>
2019-08-13 19:45:23 -0400

    branch commit test

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,32 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+// Makes the camera follow the player
 
-    public Transform target;
+public class CameraController : MonoBehaviour {
 
-    public Vector3 offset;
-    public float zoomSpeed =4f;
-    public float minZoom = 5f;
-    public float maxZoom = 15f;
-    
-    public float pitch = 2f;
-    public float yawSpeed = 100f;
-    private float currentZoom =10f;
-    private float yawInput = 0f;
-    // Start is called before the first frame update
+	public Transform target;	// Target to follow (player)
 
-    void Update (){
-        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+	public Vector3 offset;			// Offset from the player
+	public float zoomSpeed = 4f;	// How quickly we zoom
+	public float minZoom = 5f;		// Min zoom amount
+	public float maxZoom = 15f;		// Max zoom amount
 
-        yawInput += Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
-    }
-    void LateUpdate (){
-        transform.position = target.position - offset * currentZoom;
-        transform.LookAt(target.position + Vector3.up * pitch);
-        transform.RotateAround(target.position, Vector3.up, yawInput);
-    }
+	public float pitch = 2f;		// Pitch up the camera to look at head
+
+	public float yawSpeed = 100f;	// How quickly we rotate
+
+	// In these variables we store input from Update
+	private float currentZoom = 10f;
+	private float currentYaw = 0f;
+
+	void Update ()
+	{
+		// Adjust our zoom based on the scrollwheel
+		currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+		currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+		// Adjust our camera's rotation around the player
+		currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+	}
+
+	void LateUpdate ()
+	{
+		// Set our cameras position based on offset and zoom
+		transform.position = target.position - offset * currentZoom;
+		// Look at the player's head
+		transform.LookAt(target.position + Vector3.up * pitch);
+
+		// Rotate around the player
+		transform.RotateAround(target.position, Vector3.up, currentYaw);
+	}
+
 }
